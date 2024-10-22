a1a2807637305d9cbdc228e90de083271eb802d6
Yuri Silva <yrgamerbr@gmail.com>
2024-10-22 09:34:07 -0300

    feat: adds header

--- src/components/nav/header.tsx
+++ src/components/nav/header.tsx
@@ -1,9 +1,38 @@
-import { FC } from "react";
+import Link from "next/link";
 
-interface HeaderProps {}
+import Graphics from "@/components/Graphics";
+import { Button } from "@/components/ui/button";
 
-const header: FC<HeaderProps> = ({}) => {
-  return <div>header</div>;
+const Header = () => {
+    return (
+        <header className="px-4 py-2 md:px-20 md:py-6">
+            <div className="flex justify-between">
+                <div className="flex items-center gap-[2.375rem]">
+                    <Link href="/" className="text-[2rem] font-bold">
+                        Soller
+                    </Link>
+                    <nav className="hidden items-center gap-4 font-medium md:flex">
+                        <Link href="/products">Products</Link>
+                        <Link href="/solutions">Solutions</Link>
+                        <Link href="/services">Services</Link>
+                        <Link href="/configure">Configure</Link>
+                    </nav>
+                </div>
+                <div className="hidden items-center gap-4 md:flex">
+                    <Button variant="ghost" className="gap-4">
+                        <Graphics.headset />
+                        <span className="text-base text-tertiary-foreground">
+                            555 818 282
+                        </span>
+                    </Button>
+                    <Button variant="outline" size="lg">
+                        <span className="px-6">Request a Quote</span>
+                        <Graphics.arrowRight />
+                    </Button>
+                </div>
+            </div>
+        </header>
+    );
 };
 
-export default header;
+export default Header;
