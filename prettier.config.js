326f376af0113106e4898dd92ac4ef899066d1a2
Yuri Silva <yrgamerbr@gmail.com>
2024-10-22 08:11:31 -0300

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+  trailingComma: "es5",
+  tabWidth: 4,
+  semi: true,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  plugins: ["prettier-plugin-tailwindcss"]
+};
+
+export default config;
---
+++ src/components/nav/header.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface HeaderProps {}
+
+const header: FC<HeaderProps> = ({}) => {
+  return <div>header</div>;
+};
+
+export default header;
