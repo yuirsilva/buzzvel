7747e8bd7d5e43b1158bcbe5f2d264b208d27ba0
Yuri Silva <yrgamerbr@gmail.com>
2024-10-23 19:21:00 -0300

    fix: twitter-image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export const metadata: Metadata = {
         card: "summary_large_image",
         title: siteConfig.name,
         description: siteConfig.description,
-        images: [`${siteConfig.url}/default_opengraph.png`],
+        images: [`${siteConfig.url}/opengraph-image.png`],
         creator: "@yidxte",
     },
 };
